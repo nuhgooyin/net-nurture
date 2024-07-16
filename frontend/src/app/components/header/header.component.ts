32bec763ea48ffac42947c109b577d2f50a28e58
WilsonUTrash <luoweitan44@gmail.com>
2024-07-16 10:19:43 -0400

    route-guard: manage the visibility of the buttons based on the gmail linking status

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { GoogleAuthService } from '../../services/google-auth.service';
 
 @Component({
   selector: 'app-header',
@@ -11,16 +12,21 @@ import { AuthService } from '../../services/auth.service';
 export class HeaderComponent implements OnInit {
   error: string = '';
   isLoggedIn = false;
+  isGoogleAuthenticated = false;
 
   constructor(
     private api: ApiService,
     private router: Router,
     private authService: AuthService,
+    private googleAuthService: GoogleAuthService,
   ) {}
 
   ngOnInit(): void {
     this.authService.isLoggedIn().subscribe((status) => {
       this.isLoggedIn = status;
+    });
+    this.googleAuthService.isGoogleAuthenticated().subscribe((status) => {
+      this.isGoogleAuthenticated = status;
     });
   }
 
