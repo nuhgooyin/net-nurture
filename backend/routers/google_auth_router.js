afaeadd574817c2a67e929c528fb9527b6876a05
WilsonUTrash <luoweitan44@gmail.com>
2024-07-16 04:37:44 -0400

    navigation-flow: add authentication to gmail linking and store the email to the user table

--- backend/routers/google_auth_router.js
+++ backend/routers/google_auth_router.js
@@ -4,9 +4,10 @@ import {
   verifyGoogleCode,
   signOut,
 } from "../controllers/googleAuthController.js";
+import { authenticate } from "../middleware/authenticate.js";
 
 export const googleAuthRouter = Router();
 
 googleAuthRouter.post("/verify-token", verifyGoogleToken);
 googleAuthRouter.post("/signout", signOut);
-googleAuthRouter.post("/verify-code", verifyGoogleCode);
+googleAuthRouter.post("/verify-code", authenticate, verifyGoogleCode);
